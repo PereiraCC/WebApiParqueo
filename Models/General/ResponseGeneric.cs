013cdba2dd2704930e2264a79567039f540d1b4d
Carlos Pereira <carloscoto0103@gmail.com>
2023-10-15 16:16:26 -0600

    Fisrt Commit: se inicia el proyecto de WebApi

---
+++ Models/General/ResponseGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.General
+{
+    public class ResponseGeneric<T> : Response
+    {
+        public T Responses { get; set; }
+        public ResponseGeneric(T response) : base()
+        {
+            this.Responses = response;
+        }
+        public ResponseGeneric(Exception currentException) : base(currentException)
+        {
+            this.Responses = default(T);
+        }
+        public ResponseGeneric(string currentException) : base(currentException)
+        {
+            this.Responses = default(T);
+        }
+    }
+}
